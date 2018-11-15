fb0abc21d5370826d1e42f352bcd3a7abf946e23
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2018-11-15 17:18:24 +0100

    Add a tool for setting up the toolchain

---
+++ setenv.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+
+
+taffo_setenv_find()
+{
+  cd $1
+  BASEDIR=$(pwd)
+  
+  if [[ $2 == 'lib' ]]; then
+    SOEXT="so"
+    if [ $(uname -s) = "Darwin" ]; then
+      SOEXT="dylib";
+    fi
+    FN="$3.$SOEXT"
+  else
+    FN="$3"
+  fi
+  
+  PATH="$BASEDIR/$2/$FN"
+  
+  if [[ ! -e "$PATH" ]]; then
+    echo "Cannot find $FN" > /dev/stderr
+  else
+    echo "Found $PATH" > /dev/stderr
+    echo "$PATH"
+  fi
+}
+
+
+if [[ $# < 1 ]]; then
+  echo 'Usage: source setenv.sh /prefix/where/taffo/was/installed'
+  echo 'Install taffo by running make install. The default prefix is /usr/local.'
+else
+  export INITLIB=$(taffo_setenv_find $1 'lib' 'TaffoInitializer')
+  export PASSLIB=$(taffo_setenv_find $1 'lib' 'LLVMFloatToFixed')
+  export ERRORLIB=$(taffo_setenv_find $1 'lib' 'LLVMErrorPropagator')
+  export INSTCNT=$(taffo_setenv_find $1 'bin' 'istr_type')
+  export INSTMIX="$INSTCNT"
+fi
+
+unset -f taffo_setenv_find
