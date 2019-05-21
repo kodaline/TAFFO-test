42bda558db0c8de4a4e94059b5172c4d10477e0c
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2019-05-21 16:59:09 +0200

    Start general reset of the polybench port

---
+++ polybench-c-4.2.1-beta/compile.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM SIGKILL
+
+SCRIPTPATH=$(dirname "$BASH_SOURCE")
+cd "$SCRIPTPATH"
+
+TIMEOUT='timeout'
+if [[ -z $(which $TIMEOUT) ]]; then
+  TIMEOUT='gtimeout'
+fi
+if [[ ! ( -z $(which $TIMEOUT) ) ]]; then
+  TIMEOUT="$TIMEOUT 20"
+else
+  printf 'warning: timeout command not found\n'
+  TIMEOUT=''
+fi
+
+
+compile_one()
+{
+  benchpath=$1
+  xparams=$2
+  benchdir=$(dirname $benchpath)
+  benchname=$(basename $benchdir)
+  ../magiclang2.sh \
+    -o build/"$benchname".out \
+    -float-output build/"$benchname".float.out \
+    "$benchpath" \
+    ./utilities/polybench.c \
+    -I"$benchdir" \
+    -I./utilities \
+    -I./ \
+    $xparams \
+    -debug-taffo
+}
+
+
+D_MINI_DATASET="MINI_DATASET"
+D_SMALL_DATASET="SMALL_DATASET"
+D_STANDARD_DATASET="MEDIUM_DATASET"
+D_LARGE_DATASET="LARGE_DATASET"
+D_EXTRALARGE_DATASET="EXTRALARGE_DATASET"
+D_M=0
+D_DATA_TYPE='DATA_TYPE_IS_FLOAT'
+ONLY='.*'
+FRAC=''
+TOT='32'
+TBLDUMP=0
+LISTDUMP=0
+D_CONF="CONF_GOOD"
+
+for arg; do
+  case $arg in
+    64bit)
+      D_M=2
+      D_DATA_TYPE='DATA_TYPE_IS_DOUBLE'
+      ;;
+    [A-Z]*_DATASET)
+      D_MINI_DATASET=$arg
+      D_SMALL_DATASET=$arg
+      D_STANDARD_DATASET=$arg
+      D_LARGE_DATASET=$arg
+      D_EXTRALARGE_DATASET=$arg
+      ;;
+    CONF_[A-Z]*)
+      D_CONF=$arg
+      ;;
+    --only=*)
+      ONLY="${arg#*=}"
+      ;;
+    --tot=*)
+      TOT="${arg#*=}"
+      ;;
+    *)
+      echo Unrecognized option $arg
+      exit 1
+  esac
+done
+
+mkdir -p build
+rm -f build.log
+
+all_benchs=$(cat ./utilities/benchmark_list)
+for bench in $all_benchs; do
+  if [[ "$bench" =~ $ONLY ]]; then
+    printf '[....] %s' "$bench"
+    compile_one "$bench" "-D$D_CONF -D$D_STANDARD_DATASET -Xdta -totalbits -Xdta $TOT" 2>> build.log
+    bpid_fc=$?
+    if [[ $bpid_fc == 0 ]]; then
+      bpid_fc=' ok '
+    fi
+    printf '\033[1G[%4s] %s\n' "$bpid_fc" "$bench"
+  else
+    printf '[SKIP] %s\n' "$bench"
+  fi
+done
+
+
--- polybench-c-4.2.1-beta/compile_medley.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source common_compile.sh
-
-ROOT='medley'
-compile "deriche" $D_STANDARD_DATASET         29 32 61 64
-compile "floyd-warshall" $D_STANDARD_DATASET  20 32 52 64
-compile "nussinov" $D_STANDARD_DATASET        23 32 31 64
